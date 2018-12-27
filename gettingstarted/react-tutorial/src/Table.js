859bfef42d473e7503421d1dd2e987899e9fb865
Peter Bødskov <pb@reeltrak.com>
2018-12-27 14:45:19 +0100

    now loading jobs from JSON

--- gettingstarted/react-tutorial/src/Table.js
+++ gettingstarted/react-tutorial/src/Table.js
@@ -11,34 +11,26 @@ const TableHeader = () => {
   );
 }
 
-const TableBody = () => {
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return(
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    );
+  });
   return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
-      </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
+    <tbody>{rows}</tbody>
   );
 }
 class Table extends React.Component {
   render() {
+    const { characterData } = this.props;
     return(
       <table>
         <TableHeader />
-        <TableBody />
+        <TableBody characterData={characterData}/>
       </table>
     );
   }
